1aab360bef1aa4488f3d77bab82ca57a0de6359f
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-10-01 14:34:03 -0400

    allowed cors

--- server.js
+++ server.js
@@ -6,10 +6,19 @@ const port = 3000;
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 const pollingData = (streetNumber, streetName) =>
   `https://findmypollsite.vote.nyc/api/pollsiteinfo?county=Queens&streetnumber=${streetNumber}&streetname=${streetName}`;
 
-app.get('/api/v1/pollsite/:number/:name', async (req, res) => {
+app.get('/api/v1/pollsite/:number/:name', async (req, res, next) => {
   await axios
     .get(pollingData(req.params.number, req.params.name), {
       headers: {
